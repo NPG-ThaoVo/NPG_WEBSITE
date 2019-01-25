9786e90bf09229da7845378d0a7d6466f56ce08a
NPG-ThaoVo <thao.vo@napaglobal.com>
2019-01-25 16:02:00 +0700

    Revert "Update mail.php"
    This reverts commit 89864c37981ebcf4738456ccf70e4af3480e2492.

--- mail.php
+++ mail.php
@@ -43,17 +43,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //Server settings
         $mail->SMTPDebug = 0;                                 // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'mail92100.maychuemail.com';  // Specify main and backup SMTP servers
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'contact@napaglobal.com';                 // SMTP username
-        $mail->Password = 'napa@email2018';                           // SMTP password
-        $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 465;                                    // TCP port to connect to
+        $mail->Username = 'xxx';                 // SMTP username
+        $mail->Password = 'xxx';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
     
         //Recipients
     
         $mail->setFrom($email, $name);
-        $mail->addAddress('long@napaglobal.com', 'System');     // Add a recipient
+        $mail->addAddress('xxx', 'System');     // Add a recipient
     
         //Content
         $mail->isHTML(true);                                  // Set email format to HTML
